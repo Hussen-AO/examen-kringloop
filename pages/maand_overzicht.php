21d60ac575f2676243212579b4b6dda127419d59
Hussen-AO <hussenAO@hotmail.com>
2026-01-29 11:22:09 +0100

    comments maandoverzicht

--- pages/maand_overzicht.php
+++ pages/maand_overzicht.php
@@ -1,22 +1,37 @@
 <?php
+/*
+Naam script     : maand_overzicht.php
+Versie          : 1.0
+Datum           : 29-01-2026
+Beschrijving    : Toont de totale omzet per maand
+Auteur          : 
+*/
+
+// Controleer of gebruiker ingelogd is en heeft juiste rol
 require_once "../config/auth_check.php";
 requireDirectie();
 
+// Database en klasse laden
 require_once "../config/Database.php";
 require_once "../klasses/Verkoop.php";
 
+// Database verbinding maken
 $db = new Database();
 $conn = $db->connect();
 
+// Verkoop object aanmaken
 $verkoop = new Verkoop($conn);
 
+// Jaar en maand ophalen uit GET parameters, standaard huidige maand/jaar
 $jaar = (int)($_GET["jaar"] ?? date("Y"));
 $maand = (int)($_GET["maand"] ?? date("n"));
 
+// Validatie: zorg dat maand tussen 1 en 12 is
 if ($maand < 1 || $maand > 12) {
     $maand = (int)date("n");
 }
 
+// Haal omzet op voor geselecteerde maand en jaar
 $omzet = $verkoop->getOmzetMaand($jaar, $maand);
 ?>
 <!DOCTYPE html>
